b97581280cbe49f13a6ff9bf7c63cc5de0849ef3
Soundarya Murugan <soundaryamurugan2002@gmail.com>
2024-11-27 13:52:17 +0530

    added

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 
 
-from flask import Flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html', name="Soundarya")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
