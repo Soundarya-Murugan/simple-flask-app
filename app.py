08c55470411151e3842a5bd7969d6d7a7d27812f
Soundarya Murugan <soundaryamurugan2002@gmail.com>
2024-11-27 15:14:03 +0530

    logs added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ source:
     http://127.0.0.1:5000/reverse?name=Raji
 '''
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -21,21 +21,21 @@ def home():
 
     return f"Welcome to Toronto {r_name}"
 
-# @app.route("/reverse")
-# def reverse_name():
+@app.route("/reverse")
+def reverse_name():
 
-#     name = request.values.get("name")
-#     r_name = name[::-1]
+    name = request.values.get("name")
+    r_name = name[::-1]
 
-#     return f"Your name is: {name}, your reverse name is: {r_name}"
+    return f"Your name is: {name}, your reverse name is: {r_name}"
 
-# @app.route("/user")
-# def get_user_details():
+@app.route("/user")
+def get_user_details():
 
-#     name    = request.values.get("name")
-#     age     = request.values.get("age")
+    name    = request.values.get("name")
+    age     = request.values.get("age")
 
-#     return f"Your name is: {name}, your age is {age}"
+    return f"Your name is: {name}, your age is {age}"
 
 # @app.route("/voting/eligibility")
 # def get_voting_eligibility():
@@ -50,26 +50,27 @@ def home():
 
 #     return content
 
-@app.route("/country")
-def get_country_capital():
-    
-    capitals = {
-        'France'  : 'Paris',
-        'Germany' : 'Berlin' ,
-        'India'   :  'New Delhi'
+@app.route("/country", methods=["GET", "POST"])
+def country():
+    country = None
+    capital = None
+    error = None
+
+    if request.method == "GET":
+        country = request.args.get("country")
         
-    }
-   # Get the country name from the request arguments
-    country = request.args.get("country")
+        if country:
+            capitals = {
+                'France'  : 'Paris',
+                'Germany' : 'Berlin',
+                'India'   : 'New Delhi'
+            }
 
-    # Look up the capital and return it if it exists, otherwise return a message
-    capital = capitals.get(country)
-    if capital:
-        return jsonify({"country": country, "capital": capital})
-    else:
-        return jsonify({"error": "Country not found"}), 404
+            capital = capitals.get(country)
+            if not capital:
+                error = "Country not found"
+    
+    return render_template("login.html", country=country, capital=capital, error=error)
 
-if __name__ == '__main__':
-    app.run(
-        debug=True
-    )
+if __name__ == "__main__":
+    app.run(debug=True)
